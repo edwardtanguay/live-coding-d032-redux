23f277fd308213b66b77fbc7d9bf6871ca32c463
Edward Tanguay (windows) <edward@tanguay.info>
2022-07-01 15:13:06 +0200

    redux is successfully used in two components

--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -1,8 +1,14 @@
+import { useDispatch } from 'react-redux';
+import { addBook, removeBook } from "../features/cart/cartSlice";
+
 export const PageBooks = () => {
+	const dispatch = useDispatch();
+
 	return (
 		<div className="page_books">
 			<h2>Books</h2>
-			<p>This is the book page.</p>
+			<button onClick={() => dispatch(addBook())}>Add book</button>
+			<button onClick={()=> dispatch(removeBook())}>Remove book</button>
 		</div>
 	)
 }
--- src/pages/PageCart.jsx
+++ src/pages/PageCart.jsx
@@ -1,8 +1,12 @@
+import { useSelector } from 'react-redux';
+
 export const PageCart = () => {
+	const { bookCounter } = useSelector((state) => state.cart);
+
 	return (
 		<div className="page_cart">
 			<h2>Cart</h2>
-			<p>the cart page</p>
+			<p>There are {bookCounter} books.</p>
 		</div>
 	)
 }
