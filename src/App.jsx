3036bf8e33929ff1d0bd754c1f2ca4d616e2797d
Edward Tanguay (windows) <edward@tanguay.info>
2022-07-01 16:09:20 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,28 @@ import { PageWelcome } from './pages/PageWelcome';
 import { PageBooks } from './pages/PageBooks';
 import { PageCart } from './pages/PageCart';
 import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+
+const url = 'https://edwardtanguay.netlify.app/share/techBooks.json';
 
 function App() {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		(async () => {
+			const _books = (await axios.get(url)).data;
+			_books.forEach((book) => {
+				const randomPrice = Math.floor(Math.random() * 20) + 20;
+				dispatch({
+					type: 'cart/addBook',
+					payload: { title: book.title, price: randomPrice },
+				});
+			});
+		})();
+	}, []);
+
 	return (
 		<div className="App">
 			<h1>Book Store App</h1>
@@ -17,7 +37,7 @@ function App() {
 				<Route path="/welcome" element={<PageWelcome />} />
 				<Route path="/books" element={<PageBooks />} />
 				<Route path="/cart" element={<PageCart />} />
-				<Route path="/" element={<Navigate to="/welcome" replace />}/>
+				<Route path="/" element={<Navigate to="/welcome" replace />} />
 			</Routes>
 		</div>
 	);
